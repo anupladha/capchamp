46105fbf5d54eaa9079f9997143e15bb1545b794
Anup Ladha <anup.ladha@gmail.com>
2026-01-30 08:11:57 +0000

    rest protocol

--- srv/service.cds
+++ srv/service.cds
@@ -1,3 +1,9 @@
+@protocol: 'rest'
 service product {
     function printhelloworld (input: String) returns String;
+    function addition (num1: Integer, num2: Integer) returns Integer;
+    function subtraction (num1: Integer, num2: Integer) returns Integer;
+    @open
+    type object {};
+    function myFunction (category: Integer) returns object;
 }
--- srv/service.js
+++ srv/service.js
@@ -5,4 +5,36 @@ module.exports = cds.service.impl(async function(srv) {
         console.log(req.data.input) //print the request coming from server
         return `${req.data.input} World`
     })
+
+    srv.on('addition', req => {
+        console.log(req.data)
+        let result = req.data.num1 + req.data.num2
+        return result
+    })
+
+    srv.on('subtraction', req => {
+        console.log(req.data)
+        let result = req.data.num1 - req.data.num2
+        return result
+    })
+
+    srv.on('myFunction', req => {
+        let result = {}
+
+        if (req.data.category == 1) {
+            result.product = 'BMW',
+            result.price = '1200 USD',
+            result.location = 'New Delhi'
+        } else {
+            result.product = 'Audi',
+            result.stock = 120,
+            result.priceArray = [
+                {
+                    "Price": 47457,
+                    "Discount": "20 %"
+                }                
+            ]
+        }
+        return result
+    })
 })
